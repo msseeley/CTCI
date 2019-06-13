43f0fccb7df21eee585b8333dc0627fd8493793b
Molly Seeley <mseeley89@gmail.com>
2019-06-12 20:35:46 -0500

    my solution, CTCI solution 1

--- 4-trees and graphs/4.12-pathWithSum.js
+++ 4-trees and graphs/4.12-pathWithSum.js
@@ -4,6 +4,7 @@ You are given a binary tree in which each node contains an integer value (which 
 */
 
 // My solution:
+// Time complexity: O (log n) to 0 (n^2)
 
 const add = (a, v) => a.map(n => n + v);
 
@@ -22,3 +23,35 @@ function possiblePaths(bt, goal) {
   const possibleSums = recursiveSum(bt, goal);
   return (check(possibleSums, goal).length + saved.length);
 }
+
+////////////////////////////////////////////////////////////////////////////////////
+
+//CTCI Solution 1 - Brute Force:
+const countPathsWithSumFromNode = (node, targetSum, currentSum) => {
+  if (node !== null) return 0;
+  currentSum += node.value;
+
+  let totalPaths = 0;
+  if (currentSum == targetSum) {
+    totalPaths++
+  }
+  totalPaths += countPathsWithSumFromNode(node.left, targetSum, currentSum);
+  totalPaths += countPathsWithSumFromNode(node.right, targetSum, currentSum);
+  return totalPaths;
+}
+
+const countPathsWithSumFromNode = (node, targetSum) => {
+  if (node === null) return 0;
+
+  const pathsFromRoot = countPathsWithSumFromNode(node, targetSum, 0);
+  const pathsOnLeft = countPathsWithSumFromNode(node.left, targetSum);
+  const pathsOnRight = countPathsWithSumFromNode(node.right, targetSum);
+
+  return pathsFromRoot + pathsOnLeft + pathsOnRight;
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+
+//CTCI Solution 2 - Optimized:
+
+
