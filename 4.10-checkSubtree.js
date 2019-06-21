8015fe4277e9321192b6c257ec4c69f1e2664d23
Molly Seeley <mseeley89@gmail.com>
2019-06-21 10:48:22 -0500

    time complexity for my solution

--- 4.10-checkSubtree.js
+++ 4.10-checkSubtree.js
@@ -6,13 +6,13 @@ T1 and T2 are two very large binary trees, with T1 being much bigger than T2. Cr
 
 
 //My Solution:
-//Time complexity: ??
+//Time complexity: O (n * m)
 
 function checkSubtree(lT, sT) {
-  const larger = lT.travers();
+  const larger = lT.traverse();
   for (const node of larger) {
     if (node.data === sT.data) {
-      if (matching(node, sT)) {
+      if (matching(node, sT)) { //we only want to stop the loop and return true if a subtree is found
         return true;
       }
     }
@@ -27,4 +27,12 @@ function matching(t1, t2) {
     return matching(t1.left, t2.left) && matching(t1.right, t2.right);
   }
   return false;
+}
+
+//creating a method for the tree, generator function for traversal
+//makes it lazy so it's possible to go through the tree and keep trying
+traverse(){
+  yield this;
+  if (this.left) yield * this.left.traverse();
+  if (this.right) yield * this.right.traverse();
 }
